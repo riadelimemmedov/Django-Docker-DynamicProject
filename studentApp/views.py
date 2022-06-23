a8a6db02f9c144246790282e70138b7c8378eb81
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-06-23 09:08:56 +0400

    Changes views file

--- studentApp/views.py
+++ studentApp/views.py
@@ -5,5 +5,5 @@ from .models import *
 
 class StudentListView(ListView):
     model = Student
-    template_name = 'students.html'#send all studends in students.html
+    template_name = 'students.html'#send all studends in students.html and js
     context_object_name = 'students'
